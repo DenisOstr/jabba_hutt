3078069595c7110a16548a030b3fe007c40a20e3
denis <denisostrovskis1@gmail.com>
2024-10-23 21:48:23 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <Application
+    :width="windowWidth"
+    :height="windowHeight"
+    backgroundColor="#fff"
+  >
+    <BaseComponent :width="windowWidth" :height="windowHeight" />
+  </Application>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { Application } from 'vue3-pixi'
+import BaseComponent from './components/BaseComponent.vue'
+
+const windowWidth = computed(() => {
+  return window.innerWidth
+})
+
+const windowHeight = computed(() => {
+  return window.innerHeight
+})
+</script>
+
+<style>
+body {
+  padding: 0;
+  margin: 0;
+}
+</style>
---
+++ src/components/BaseComponent.vue
@@ -0,0 +1,10 @@
+<template>
+  <text :anchor="0.5" :x="width / 2" :y="height / 2">Welcome</text>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  width: number
+  height: number
+}>()
+</script>
